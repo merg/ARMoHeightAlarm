1a80e4efde9033bf6b0d8b1db6a7c2fc08ad4889
demerg <demerg@me.com>
2012-01-24 18:30:14 +0100

    Initial checkin

---
+++ ARMoHeightAlarm.ino
@@ -0,0 +1,201 @@
+#define heightParam 39
+#define gpsNorthParam 22
+#define frameBlink 20
+#define alarmCheckInterval 3000
+
+int altAlarm = 4; // set this to 100m :-)
+
+int infoPin = 10;
+int info2Pin = 11;
+int errPin = 12;
+int buzzPin = 13;
+boolean infoOn = false;
+boolean info2On = false;
+boolean errOn = false;
+
+byte f[7]; // buffer to hold a complete debug frame
+int fc; // framecounter - reference to where the frame has been filled up
+byte b=0; // last read byte from serial
+int lastDebugParm=0;
+long framesParsed = 0; // Number of total frames parsed
+long debugValues[256]; // Contains ALL the parsed debug values
+unsigned long running = 0; // The 'clock' in msec elapsed
+unsigned long lastHeightFrame = 0;
+unsigned long lastAlarm = 0;
+int heightFrameAlarm = 2000; // If no heightframe has been parsed for this amount of msec, sound the alarm
+boolean noHeightFrames = false;
+
+void setup() {
+  pinMode(infoPin, OUTPUT);
+  pinMode(info2Pin, OUTPUT);
+  pinMode(errPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+
+  Serial.begin(38400); // 38400,8,N,1 default Arduino  
+  setInitLEDS();
+  String s = "GO";
+  Serial.println(s);
+
+  // Initialize the debugvalues to 0xFF
+  for(int i = 0; i < 256; i++) {
+    debugValues[i] = 0xFF;
+  }
+
+  lastHeightFrame = millis();
+}
+
+void loop() {
+  if(Serial.available()) {
+    b = Serial.read();
+    if(b == 0x1c) {
+      fc=0;
+      f[fc++] = b;
+      readFrame();
+      if(parseFrame()) {
+        ledFeedback();
+        checkHeight();
+        checkGPSNorth();
+      }
+    }
+  }
+  running = millis();
+  checkAlarms();
+}
+
+void checkAlarms() {
+  if((running - lastHeightFrame) > heightFrameAlarm) {
+    noHeightFrames = true;
+  } 
+  else {
+    noHeightFrames = false;
+  }
+
+  if(noHeightFrames) {
+    setAlarmLEDS();
+    if((running - lastAlarm) > alarmCheckInterval) {
+      tone(buzzPin, 5000, 100);
+      lastAlarm = millis();
+    }
+  }
+}
+
+void ledFeedback() {
+  if((framesParsed %  frameBlink) == 0) {
+    digitalWrite(infoPin, (infoOn ? LOW : HIGH));
+    infoOn = !infoOn;
+  }
+}
+
+void readFrame() {
+  while(fc < 7) {
+    if(Serial.available() > 0) {
+      b = Serial.read();
+      f[fc++] = b;
+    }
+  }
+}
+
+boolean parseFrame() {
+  long crc = f[1] ^ f[2] ^ f[3]^ f[4]^ f[5];
+  if(crc==f[6]) {    
+    if((f[1] >= 0) && (f[1] < 256)) {
+      long val = 0;
+      val = (val <<8)+ ((long)f[5]&0xff);
+      val = (val <<8)+ ((long)f[4]&0xff);
+      val = (val <<8)+ ((long)f[3]&0xff);
+      val = (val <<8)+ ((long)f[2]&0xff);
+
+      //      Serial.print("VAL: ");
+      //      Serial.print(f[1], DEC);
+      //      Serial.print(" [");
+      //      Serial.print(val);
+      //      Serial.print("]: ");
+      //      printFrame();
+      debugValues[f[1]] = val;
+      framesParsed++;
+      lastDebugParm = f[1];
+      return true;
+    } 
+  }
+  else {
+    //Serial.print("INV: ");
+    //printFrame();
+  }
+  return false;
+}
+
+
+void checkHeight() {
+  if(lastDebugParm==heightParam) {
+    lastHeightFrame = millis();
+    if(debugValues[heightParam] != 0xFF) {
+      int height =debugValues[heightParam] * 20 / 1000;
+      digitalWrite(info2Pin, (info2On ? LOW : HIGH));
+      info2On = !info2On;
+
+      if(height >= altAlarm) {
+        tone(buzzPin, 2000, 100);        
+        delay(20);
+        tone(buzzPin, 5000, 200);
+        delay(20);
+        tone(buzzPin, 1000, 100);
+        digitalWrite(errPin, HIGH);
+      } 
+      else {
+
+        digitalWrite(errPin, LOW);
+
+      }
+
+
+      Serial.print("**** Height: ");
+      Serial.print(height);
+      Serial.print (" (");
+      Serial.print(debugValues[heightParam]);
+      Serial.print (")\n");
+    }
+  }
+}
+
+void checkGPSNorth() {
+  if(lastDebugParm==gpsNorthParam) {
+    if(debugValues[gpsNorthParam] != 0xFF) {
+      Serial.print("**** GPS North: ");
+      Serial.println (debugValues[gpsNorthParam]);
+    }
+  }
+}
+
+void printFrame() {
+  for(int i = 0; i < 7; i++) {
+    Serial.print(f[i], BIN);
+    Serial.print (" ");
+  }
+  Serial.println("");
+}
+
+void setInitLEDS() {
+  digitalWrite(infoPin, HIGH);
+  infoOn=true;
+  digitalWrite(info2Pin, LOW);
+  infoOn=false;
+  digitalWrite(errPin, LOW);
+  errOn= false;
+}
+
+void setAlarmLEDS() {
+  digitalWrite(infoPin, LOW);
+  infoOn=false;
+  digitalWrite(info2Pin, LOW);
+  infoOn=false;
+  digitalWrite(errPin, HIGH);
+  errOn= true;
+}
+
+
+
+
+
+
+
+
